87b571777c88166701a8b93d992f59ddab09adaf
IoLux <yuriaan856@gmail.com>
2021-10-30 21:59:56 +0700

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+  res.send("HELLo");
 });
 
 app.listen(process.env.PORT || 3000, () => {
