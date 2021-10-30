3e13463471a40379b73d0fc1bf532a03f9a9ef77
IoLux <yuriaan856@gmail.com>
2021-10-30 19:27:37 +0700

    a

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res, next) => {
-  res.send("HELLO");
+// use the express-static middleware
+app.use(express.static("public"));
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(port, () => {
