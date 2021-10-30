78ad4bc4234d63cd69d7be8c2b66008cd559f7bf
IoLux <yuriaan856@gmail.com>
2021-10-30 22:44:07 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const data = require("./json/postUser.json");
+const postUser = require("./json/postUser.json");
+const users = require("./json/user.json");
 // const port = 5000;
 
 // define the first route
 app.get("/postUser", function (req, res) {
-  res.json(data);
+  res.json(postUser);
+});
+
+app.get("/user", (req, res, next) => {
+  res.json(users);
 });
 
 app.listen(process.env.PORT || 3000, () => {
